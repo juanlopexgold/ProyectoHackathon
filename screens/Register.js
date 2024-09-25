1aa485ddba85e92e8267c065b2604448ca50fcd0
juanlopexgold <juanlopexdhegold@gmail.com>
2024-09-25 14:46:15 -0600

    actualizaciones

--- screens/Register.js
+++ screens/Register.js
@@ -1,4 +1,4 @@
-import { SafeAreaView } from 'react-native';
+import { SafeAreaView, ScrollView, View } from 'react-native';
 import { BlueButtton } from '../components/Butttons';
 import { LogoImage2 } from '../components/Imagen';
 import { useNavigation } from '@react-navigation/native';
@@ -8,16 +8,18 @@ import { TextPersonalizadosSolos } from '../components/Toush';
 export default function App() {
     const navegacion = useNavigation();
     return (
-        <SafeAreaView style={{ flex: 1, justifyContent: 'center', alignItems:'center' }}>
-
-            <LogoImage2 source={require('../src/images/Group.png')}/>
-            <TextInputPer text="Cedula"/>
-            <TextInputPer text="Email"/>
-            <TextInputPer text="Fecha de nacimiento"/>
-            <TextInputPerButt text="Direccion"/>
-            <BlueButtton onPress= {() => navegacion.navigate("Home")} text="Inscribirse"/>
-            <TextPersonalizadosSolos onPress= {() => navegacion.navigate("Login")} text="¿Ya eres miembro? Iniciar secion"/>
-
-        </SafeAreaView>
+        <View style={{ flex: 1, justifyContent: 'center', padding: 'center' }}>
+            <SafeAreaView>
+                <LogoImage2 source={require('../src/images/Group.png')} />
+                <TextInputPer text="Cedula" />
+                <TextInputPer text="Email" />
+                <TextInputPer text="Fecha de nacimiento" />
+                <TextInputPer text="Direccion" />
+                <TextInputPer text="Usuario" />
+                <TextInputPerButt text="Contraseña" />
+                <BlueButtton onPress={() => navegacion.navigate("Home")} text="Inscribirse" />
+                <TextPersonalizadosSolos onPress={() => navegacion.navigate("Login")} text="¿Ya eres miembro? Iniciar secion" />
+            </SafeAreaView>
+        </View>
     );
 }
